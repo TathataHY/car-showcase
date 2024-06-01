de3f820e6a4e2255f7eae94d6ed0faab3706fe8c
Tathata <86846618+TathataHY@users.noreply.github.com>
2024-06-01 19:12:29 -0400

    car showcase project

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+export { default as CarCard } from "./car-card";
+export { default as CarDetails } from "./car-details";
+export { default as CustomButton } from "./custom-button";
+export { default as CustomFilter } from "./custom-filter";
+export { default as Footer } from "./footer";
+export { default as Hero } from "./hero";
+export { default as NavBar } from "./navbar";
+export { default as SearchBar } from "./search-bar";
+export { default as SearchManufacturer } from "./search-manufacturer";
+export { default as ShowMore } from "./show-more";
+
